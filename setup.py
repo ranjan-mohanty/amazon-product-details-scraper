8e070c65ef690929019be848d7d8d59a6f61ab84
Ranjan Mohanty <ranjan@duck.com>
2024-03-18 09:57:58 +0100

    Update the version for stable release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="amazon-product-details-scraper",
-    version="1.0.2-rc2",
+    version="1.0.3",
     description="Scrapes product details from Amazon product pages and also downloads the images",
     long_description=long_description,
     long_description_content_type="text/markdown",
