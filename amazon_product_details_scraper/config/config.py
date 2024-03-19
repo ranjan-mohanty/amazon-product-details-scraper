e8491b1e13b4b9a35f6009d65b7c33d16e3cff10
Ranjan Mohanty <ranjan@duck.com>
2024-03-19 07:16:11 +0100

    Fix security vulnerability

--- amazon_product_details_scraper/config/config.py
+++ amazon_product_details_scraper/config/config.py
@@ -6,3 +6,27 @@ DEFAULT_LOG_LEVEL = logging.INFO
 
 DEFAULT_OUTPUT_FILENAME = "product-info.json"
 DEFAULT_OUTPUT_FOLDER = os.path.expanduser("../output")
+
+VALID_DOMAIN_NAMES = [
+    "amazon.com",
+    "amazon.ca",
+    "amazon.co.uk",
+    "amazon.de",
+    "amazon.fr",
+    "amazon.in",
+    "amazon.it",
+    "amazon.co.jp",
+    "amazon.cn",
+    "amazon.com.mx",
+    "amazon.com.au",
+    "amazon.nl",
+    "amazon.pl",
+    "amazon.sg",
+    "amazon.sa",
+    "amazon.es",
+    "amazon.se",
+    "amazon.ae",
+    "amazon.br",
+    "amazon.com.tr",
+    "amzn.to",
+]
